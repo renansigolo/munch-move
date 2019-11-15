cae657e1e60390b3d48b04ba9b79d05175652a20
Renan Sigolo <renan.sigolo@gmail.com>
2019-11-15 17:35:18 +1100

    Adjust Layout

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Functions which enhance the theme by hooking into WordPress
  *
@@ -11,27 +12,51 @@
  * @param array $classes Classes for the body element.
  * @return array
  */
-function munch_move_body_classes( $classes ) {
+function munch_move_body_classes($classes)
+{
 	// Adds a class of hfeed to non-singular pages.
-	if ( ! is_singular() ) {
+	if (!is_singular()) {
 		$classes[] = 'hfeed';
 	}
 
 	// Adds a class of no-sidebar when there is no sidebar present.
-	if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	if (!is_active_sidebar('sidebar-1')) {
 		$classes[] = 'no-sidebar';
 	}
 
 	return $classes;
 }
-add_filter( 'body_class', 'munch_move_body_classes' );
+add_filter('body_class', 'munch_move_body_classes');
 
 /**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
-function munch_move_pingback_header() {
-	if ( is_singular() && pings_open() ) {
-		printf( '<link rel="pingback" href="%s">', esc_url( get_bloginfo( 'pingback_url' ) ) );
+function munch_move_pingback_header()
+{
+	if (is_singular() && pings_open()) {
+		printf('<link rel="pingback" href="%s">', esc_url(get_bloginfo('pingback_url')));
 	}
 }
-add_action( 'wp_head', 'munch_move_pingback_header' );
+add_action('wp_head', 'munch_move_pingback_header');
+
+/**
+ * Save Form Fields from the FrontPage to the Database
+ */
+function save_user()
+{
+	global $wpdb;
+
+	// Insert a row into a table.
+	$wpdb->insert(
+		'mm_users',
+		array(
+			'name' => 'value1',
+			'email' => '123'
+		),
+		array(
+			'%s',
+			'%d'
+		)
+	);
+}
+add_action('wp_enqueue_scripts', 'save_user');
