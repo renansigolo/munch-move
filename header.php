9568a06b20bdd9ecafc1f5ca3f1327fe16e54f35
Renan Sigolo <renan.sigolo@gmail.com>
2020-04-08 12:08:32 +1000

    feat: new footer

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
+    <!-- <link rel="stylesheet" href="https://use.typekit.net/wws1hcf.css"> -->
     <?php wp_head();?>
 </head>
 
