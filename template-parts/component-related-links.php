77cf64f775773eb6eba085ab315d156cc67c0a85
Renan Sigolo <renan.sigolo@gmail.com>
2020-05-13 14:20:14 +1000

    feat: add custom links

--- template-parts/component-related-links.php
+++ template-parts/component-related-links.php
@@ -35,3 +35,18 @@ while (have_rows('related_links_options', 'option')) : the_row();
 <?php
     endif;
 endwhile; ?>
+
+<!-- If the Category selected is Custom -->
+<?php if ($category_selected === 'Custom') : ?>
+    <aside class="related-links">
+        <h2>Related Links</h2>
+        <ul>
+            <?php
+            while (have_rows('related_links')) : the_row();
+                $link = get_sub_field('link')
+            ?>
+                <li><a href="<?php echo $link['url'] ?>"><?php echo $link['title'] ?></a></li>
+            <?php endwhile; ?>
+        </ul>
+    </aside>
+<?php endif; ?>
