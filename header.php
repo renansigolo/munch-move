90fa7f7d98e81da3b7b9c16803b250158e960123
Renan Sigolo <renan.sigolo@gmail.com>
2020-06-01 14:11:39 +1000

    fix: ie style bugs

--- header.php
+++ header.php
@@ -19,6 +19,15 @@
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
     <link rel="stylesheet" href="https://use.typekit.net/wws1hcf.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffffff">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="theme-color" content="#ffffff">
+
     <?php wp_head(); ?>
 </head>
 
