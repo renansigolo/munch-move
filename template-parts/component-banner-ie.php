96bf5b34ce6438d830844ee093822a9627cb3cc6
Renan Sigolo <renan.sigolo@gmail.com>
2020-06-18 18:51:47 +1000

    style: adjust responsive

--- template-parts/component-banner-ie.php
+++ template-parts/component-banner-ie.php
@@ -19,32 +19,32 @@ $ieBanner = get_field('show_ie_banner', 'option');
 if ($ieBanner) :
 ?>
     <section class="ie-banner" id="ie-banner">
-        <div class="container">
+        <div class="container ie-container">
             <div class="five columns">
                 <h2>Unsupported Browser Detected</h2>
                 <p>It looks like you’re using an unsupported browser to view this site. We recommend upgrading to one of the following modern browsers for a better viewing experience and security updates.</p>
             </div>
             <div class="seven columns browsers">
-                <div class="browser-logo">
+                <a href="https://www.microsoft.com/en-us/edge" target="_blank" class="browser-logo">
                     <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
                     <span>Edge</span>
-                </div>
-                <div class="browser-logo">
+                </a>
+                <a href="https://www.google.com/chrome/" target="_blank" class="browser-logo">
                     <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
-                    <span>Edge</span>
-                </div>
-                <div class="browser-logo">
+                    <span>Chrome</span>
+                </a>
+                <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank" class="browser-logo">
                     <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
-                    <span>Edge</span>
-                </div>
-                <div class="browser-logo">
+                    <span>Firefox</span>
+                </a>
+                <a href="https://www.opera.com/download" target="_blank" class="browser-logo">
                     <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
-                    <span>Edge</span>
-                </div>
-                <div class="browser-logo">
+                    <span>Opera</span>
+                </a>
+                <a href="https://www.apple.com/au/safari/" target="_blank" class="browser-logo">
                     <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
-                    <span>Edge</span>
-                </div>
+                    <span>Safari</span>
+                </a>
             </div>
         </div>
     </section>
