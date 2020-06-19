781e3bae30d4032284e3a37836335f4db0c278c5
Renan Sigolo <renan.sigolo@gmail.com>
2020-06-19 12:07:10 +1000

    fix: browser images

--- template-parts/component-banner-ie.php
+++ template-parts/component-banner-ie.php
@@ -10,11 +10,11 @@
 ?>
 
 <?php
-$logo_edge = get_template_directory_uri() . '/images/logo/edge.png';
-$logo_chrome = get_template_directory_uri() . '/images/logo/chrome.png';
-$logo_firefox = get_template_directory_uri() . '/images/logo/firefox.png';
-$logo_opera = get_template_directory_uri() . '/images/logo/opera.png';
-$logo_safari = get_template_directory_uri() . '/images/logo/safari.png';
+$logo_edge = get_template_directory_uri() . '/images/logo/browser/edge.png';
+$logo_chrome = get_template_directory_uri() . '/images/logo/browser/chrome.png';
+$logo_firefox = get_template_directory_uri() . '/images/logo/browser/firefox.png';
+$logo_opera = get_template_directory_uri() . '/images/logo/browser/opera.png';
+$logo_safari = get_template_directory_uri() . '/images/logo/browser/safari.png';
 $ieBanner = get_field('show_ie_banner', 'option');
 if ($ieBanner) :
 ?>
@@ -30,19 +30,19 @@ if ($ieBanner) :
                     <span>Edge</span>
                 </a>
                 <a href="https://www.google.com/chrome/" target="_blank" class="browser-logo">
-                    <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
+                    <img src="<?php echo $logo_chrome ?>" alt="Browser Logo">
                     <span>Chrome</span>
                 </a>
                 <a href="https://www.mozilla.org/en-US/firefox/new/" target="_blank" class="browser-logo">
-                    <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
+                    <img src="<?php echo $logo_firefox ?>" alt="Browser Logo">
                     <span>Firefox</span>
                 </a>
                 <a href="https://www.opera.com/download" target="_blank" class="browser-logo">
-                    <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
+                    <img src="<?php echo $logo_opera ?>" alt="Browser Logo">
                     <span>Opera</span>
                 </a>
                 <a href="https://www.apple.com/au/safari/" target="_blank" class="browser-logo">
-                    <img src="<?php echo $logo_edge ?>" alt="Browser Logo">
+                    <img src="<?php echo $logo_safari ?>" alt="Browser Logo">
                     <span>Safari</span>
                 </a>
             </div>
