b61606a184743c1972a1c79fe70cedb6afc90904
Renan Sigolo <renan.sigolo@gmail.com>
2020-06-23 11:57:15 +1000

    auth changes

--- js/munch-move-auth.js
+++ js/munch-move-auth.js
@@ -4,22 +4,75 @@
  * Munch & Move Auth Method
  */
 
-function authStaff() {
-  var staffAuthenticated = document.querySelector("#js-authenticated");
-  var staffError = document.querySelector(".staff-content__error");
-  var userPassword = document.querySelector(".staff-form__content input").value;
-  var staffForm = document.querySelector(".staff-form");
+var isStaff = false;
 
-  if (userPassword === "Oranges") {
+function authStaff() {
+  var staffAuthenticated = document.querySelector("#js-authenticated"),
+    staffError = document.querySelector(".staff-content__error"),
+    userPassword = document.querySelector(".staff-form__content input").value,
+    staffForm = document.querySelector(".staff-form");
+
+  userPassword === "Oranges" ? saveStaffCredentials() : showError();
+
+  function saveStaffCredentials() {
     staffError.style.display = "none";
     staffAuthenticated.style.display = "flex";
-
     staffForm.style.display = "none";
-  } else {
+    sessionStorage.setItem("isStaff", true);
+  }
+
+  function showError() {
     staffError.style.display = "flex";
   }
 }
 
-(function ($) {
-  console.log("Hello");
-})(jQuery);
+(function () {
+  var staffAuthenticated = document.querySelector("#js-authenticated"),
+    staffError = document.querySelector(".staff-content__error"),
+    staffForm = document.querySelector(".staff-form");
+
+  function showStaffContent() {
+    staffError.style.display = "none";
+    staffAuthenticated.style.display = "flex";
+    staffForm.style.display = "none";
+  }
+
+  function checkStaff() {
+    var urlPath = location.pathname;
+    var pathArr = urlPath.split("/");
+    var urlSlug = pathArr[pathArr.length - 2];
+
+    switch (urlSlug) {
+      case "staff-development-kit-and-resource-manual":
+        console.log("staff-development-kit-and-resource-manual");
+
+        break;
+      case "staff-development-kit":
+        console.log("staff-development-kit");
+        break;
+      case "munch-move-resource-manual":
+        console.log("munch-move-resource-manual");
+        break;
+
+      default:
+        break;
+    }
+
+    isStaff = sessionStorage.getItem("isStaff");
+    console.log(isStaff);
+
+    if (
+      !isStaff &&
+      urlSlug === ("staff-development-kit" || "munch-move-resource-manual")
+    ) {
+      window.location =
+        "http://munch-move-dev.local/munch-move-program-training/staff-development-kit-and-resource-manual/";
+    } else {
+      if (isStaff) {
+        showStaffContent();
+      }
+    }
+  }
+
+  checkStaff();
+})();
